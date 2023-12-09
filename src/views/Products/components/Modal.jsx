29cc0ca9b67009db9dddddaeef7475e756c03c9b
Kamenriderx <kiritojeiger@gmail.com>
2023-12-09 03:10:40 -0600

    files

--- src/views/Products/components/Modal.jsx
+++ src/views/Products/components/Modal.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Logo from "../../../assets/Image.jpeg";
+import Logo from "../../../assets/logo.png";
 import { httpRequests } from "../../../utils/httpRequest";
 
 function Modal(props) {
